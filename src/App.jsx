102eb8f359491085cbb084daeb79844134fd1f06
michaelokolo <91018269+michaelokolo@users.noreply.github.com>
2023-02-24 10:35:26 +0000

    commenced building the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import Header from "./Header"
+import Employees from "./Employees"
+import Footer from "./Footer"
 
 export default function App() {
   return (
     <div>
       <Header />
+      <Employees />
+      <Footer />
     </div>
   )
 }
