0b3f1ccff73835072eb677d81c8bf90ee1778109
michaelokolo <91018269+michaelokolo@users.noreply.github.com>
2023-02-27 08:47:33 +0000

    selection form made dynamic

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -4,6 +4,8 @@ import maleProfile from './images/maleProfile.jpg';
 
 const Employees = () => {
 
+  const [selectedTeam, setTeam] = useState('TeamB');
+
   const [employees, setEmployees] = useState([{
     id: 1,
     fullName: "Bob Jones",
@@ -89,15 +91,33 @@ const Employees = () => {
     teamName: "TeamD"
   }])
 
+  function handleTeamSelectionChange(event){
+    console.log(event.target.value)
+    setTeam(event.target.value);
+  }
+
+  
+
   return (
     <main className="container">
       <div class="row justify-content-center mt-3 mb-3">
         <div class="col-8">
-            <div>
+          <select className='form-select form-select-lg' value={selectedTeam} onChange={handleTeamSelectionChange}>
+            <option value="TeamA">TeamA</option>
+            <option value="TeamB">TeamB</option>
+            <option value="TeamC">TeamC</option>
+            <option value="TeamD">TeamD</option>
+          </select>
+        </div>
+      </div>
+      <div class="row justify-content-center mt-3 mb-3">
+        <div class="col-8">
+            <div class="card-collection">
               {
                 employees.map((employee) =>
-                  <div id={employee.id} className="card">
-                    <img src={femaleProfile} className='card-img-top' />
+                  <div id={employee.id} className="card m-2" style={{cursor:"pointer"}}>
+                    {(employee.gender === "male")?<img src={maleProfile} className='card-img-top' />:
+                      <img src={femaleProfile} className='card-img-top' />}
                     <div className="card-body">
                       <h5 className="card-title">Full Name: {employee.fullName}</h5>
                       <p className="card-text"><br/>Designation: {employee.designation}</p>
